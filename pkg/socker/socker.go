664d3d765a88c78997320062fbff6ad9e448a2dd
Tarrex <tarrex@users.noreply.github.com>
2018-06-26 13:57:30 +0800

    feat: finished images list command (#3)
    * feat: finished images list command
    
    * fix: fix name and exit code
    
    * chore: change list path to list path files content

--- pkg/socker/socker.go
+++ pkg/socker/socker.go
@@ -3,7 +3,13 @@
 // Package socker implements a secure runner for docker containers.
 package socker
 
-import "fmt"
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"path"
+)
 
 // Socker provides a runner for docker.
 type Socker struct {
@@ -14,9 +20,38 @@ func New() *Socker {
 	return &Socker{}
 }
 
-// ListImages would list all available images from your images hub.
-func (s *Socker) ListImages() {
-	fmt.Println("all images")
+// ListImages would list all available images from your images registry.
+func (s *Socker) ListImages(config string) error {
+	info, err := os.Stat(config)
+	if err != nil {
+		log.Fatal(err)
+		return err
+	}
+	if info.IsDir() {
+		files, err := ioutil.ReadDir(config)
+		if err != nil {
+			log.Fatal(err)
+			return err
+		}
+		for _, file := range files {
+			if file.IsDir() {
+				continue
+			}
+			data, err := ioutil.ReadFile(path.Join(config, file.Name()))
+			if err != nil {
+				return err
+			}
+			fmt.Println(string(data))
+		}
+	} else {
+		data, err := ioutil.ReadFile(config)
+		if err != nil {
+			log.Fatal(err)
+			return err
+		}
+		fmt.Println(string(data))
+	}
+	return nil
 }
 
 // RunImage would run a container by regular user.
