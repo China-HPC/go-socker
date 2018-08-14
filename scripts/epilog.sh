b385840df9afcc736ccc5c5bffef58fe56e64e3d
ansiz <zzz332191120@gmail.com>
2018-08-14 11:23:30 +0800

    fix: update epilog script (#30)

--- scripts/epilog.sh
+++ scripts/epilog.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-## You should configure Slurm to enable epilog. This script will be excuted 
-## after each Slurm job termnated to delete corresponding container.
-
+## You should configure Slurm to enable epilog. This script will be excuted
+## after each Slurm job termnated to delete corresponding container.
 recordFile=/var/lib/socker/epilog/$SLURM_JOB_ID
-containerName=`cat $recordFile`
-docker rm -f $containerName
-rm -f $recordFile
+if [ -f $recordFile ];then
+    echo "clean docker container for job: $SLURM_JOB_ID"
+    containerName=`cat $recordFile`
+    docker rm -f $containerName
+    rm -f $recordFile
+fi
